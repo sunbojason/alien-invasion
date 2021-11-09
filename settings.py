4ebfb92a3b3dc97f97e83b10f51a95ed37226672
sunbojason <sunbojason@foxmail.com>
2021-11-09 22:51:52 +0100

    move the alien

--- settings.py
+++ settings.py
@@ -19,7 +19,7 @@ class Settings:
         self.bg_color = (230,230,230)
 
         # ship settings
-        self.ship_speed = 1.5
+        self.ship_speed = 1.2
 
         # bullet settings
         self.bullet_speed = 1.0
@@ -27,3 +27,8 @@ class Settings:
         self.bullet_height = 15
         self.bullet_color = (60,60,60)
         self.bullets_allowed = 3
+
+        # alien settings
+        self.alien_speed = 0.8
+        self.fleet_drop_speed = 5 # the speed to drop when reach the edge
+        self.fleet_direction = 1
